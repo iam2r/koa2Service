37c5783a473261d079537ff7e868050fcaec0203
Your Name <you@example.com>
2018-06-22 13:43:04 +0800

    invoke函数独立

---
+++ controllers/invoke.js
@@ -0,0 +1,28 @@
+export default invoke = async (path, ctx, next) => {//统一获取无论get还是post请求的参数放到ctx.data上，并确定走对应的方法
+    const { object, method } = ctx.params;
+    console.log(`///invoke:${object}->${method}===${ctx.request.method}`);
+    ctx.data = { ...ctx.query, ...ctx.request.body };
+    console.log('<===========req===========>')
+    console.log(ctx.data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ctx.body = await require(`${path}${object}`)[method](ctx, next);
+    console.log('<===========res===========>')
+    console.log(ctx.body)
+}
--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,14 +1,14 @@
-const { invoke } = require('../tools/utils')
+const  invoke  = require('../controllers/invoke')
 const api = require('koa-router')({
     prefix: '/api'
 });
 
 api.get("/:object/:method", async (ctx, next) => {
-    await invoke(`../controllers/`, ctx, next)
+    await invoke(`../interfaces/`, ctx, next)
 });
 
 api.post("/:object/:method", async (ctx, next) => {
-    await invoke(`../controllers/`, ctx, next)
+    await invoke(`../interfaces/`, ctx, next)
 });
 
 module.exports = api
